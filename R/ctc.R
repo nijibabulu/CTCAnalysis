9cddf1c29156ab0a9052e20ea4a62aae3ec68e79
Bob Zimmermann <robert.paul.zimmermann@gmail.com>
2022-07-08 16:08:15 -0400

    refactoring

--- R/ctc.R
+++ R/ctc.R
@@ -1,14 +1,14 @@
 translated_factor <- function(vec, dict) factor(vec, levels=names(dict), labels=dict)
 
 # add the sample variables (patient, celltype, stage) into the data
-expand_sample_names <- function(tbl, col_name="Sample", stage_names=NULL, treatment_names=NULL, patient_type_names=NULL) {
+expand_sample_names <- function(tbl, col_name="Sample", stage_names=NULL, sample_type_names=NULL, patient_type_names=NULL) {
   tbl %>%
     tidyr::separate(!!col_name, c("Study", "Type", "Stage", "Patient"), "_", remove=F) %>%
     tidyr::separate(Patient, c("PatientType", "PatientId"), "-", remove=F) %>%
     {if(!purrr::is_null(stage_names))
       dplyr::mutate(., Stage=translated_factor(.$Stage, stage_names)) else .} %>%
-    {if(!purrr::is_null(treatment_names))
-      dplyr::mutate(., Treatment=translated_factor(.$Treatment, treatment_names)) else .} %>%
+    {if(!purrr::is_null(sample_type_names))
+      dplyr::mutate(., Type=translated_factor(.$Type, sample_type_names)) else .} %>%
     {if(!purrr::is_null(patient_type_names))
       dplyr::mutate(., PatientType=translated_factor(.$PatientType, patient_type_names)) else .}
 }
